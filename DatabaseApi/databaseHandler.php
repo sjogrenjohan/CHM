2a6f0cf946357c6be5e53318a2364d1497d09576
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-03-05 10:30:02 +0100

    Working dbHandler

--- DatabaseApi/databaseHandler.php
+++ DatabaseApi/databaseHandler.php
@@ -1,21 +1,21 @@
 <?php
 
-    class Database {
+class Database {
 
-        // Sets up a connection to the database
-        function __construct() {
-            $dsn = 'mysql:host=localhost;dbname=chmgrupp;';
-            $user = 'root';
-            $password = '';
+    // Sets up a connection to the database
+    function __construct() {
+        //$dsn = "mysql:host=localhost;dbname=chmgrupp;port=3306;charset=utf8mb4";
+        $dsn = "mysql:host=localhost;dbname=chmgrupptest1;port=3306;charset=utf8mb4";
+        $user = 'root';
+        $password = '';
 
-            try {
-
-                $this->mysgli_connect($dsn,$user,$password) //connection = new PDO($dsn, $user, $password, NULL);
-                //$this->connection->exec('set names utf8');
-            } catch (PDOException $e) {
-                throw $e;
-            }
-        } 
-    }
+        try {
+            $this->connection = new PDO($dsn, $user, $password, NULL);
+            //$this->connection->exec('set names utf8');
+        } catch (PDOException $e) {
+            throw $e;
+        }
+    } 
+}
 
 ?>
