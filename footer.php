60d60bc298aa4f4fbfbd0f2f0c21f7abacb1fd56
sjogrenjohan <sjogren_johan@hotmail.com>
2019-03-05 11:57:00 +0100

    fixed typos and tags

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
+<footer>
 <div class="container-fluid">
     <div class="row"> 
         <div class="col-lg">
@@ -7,10 +8,11 @@
             <h5>Kontakta oss: CHMcostumersupport@gmail.com</h5>
         </div>
         <div class ="col-lg"><br>
-            <a href="#">Bli Admain idag sidan</a><br>
+            <a href="#">Bli Admin idag sidan</a><br>
             <a href="#">Hur blir jag Admin?</a><br>
             <a href="#">Vilka fördelar får jag som Adimin?</a><br>
             <a href="#">Vad gör man som Admin?</a><br>
         </div>
     </div>
 </div>
+</footer>
