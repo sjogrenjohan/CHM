177cf1315ec21dd5f2263195b9e4351e48eb10a5
sjogrenjohan <sjogren_johan@hotmail.com>
2019-03-05 13:43:32 +0100

    fixed text code

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <div class="container-fluid fixed-bottom">
     <div class="row"> 
         <div class="col-sm">
-            <p class="text-lg-left">Location: Götebrg</p>
+            <p class="text-sm-left">Location: Götebrg</p>
             <p class="text-sm-left">Address: Kruthusgatan 17</p>
             <p class="text-sm-left">Kontakta oss: support@gmail.com</p>
 
