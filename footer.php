425c8ff388ec16da72afa88e3add9d4ca171a716
sjogrenjohan <sjogren_johan@hotmail.com>
2019-03-05 15:17:07 +0100

    inverting code

--- footer.php
+++ footer.php
@@ -5,7 +5,6 @@
             <p class="text-sm-left">Location: Götebrg</p>
             <p class="text-sm-left">Address: Kruthusgatan 17</p>
             <p class="text-sm-left">Kontakta oss: support@gmail.com</p>
-
         </div>
         <div class ="col-sm"><br>
             <h1 class="display-6 text-center">BILD HÄR?</h1>
