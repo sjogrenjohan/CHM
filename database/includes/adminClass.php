ee450345d4bafae75feeefa099c8344132176984
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-03-06 13:15:31 +0100

    init classes in php

---
+++ database/includes/adminClass.php
@@ -0,0 +1,37 @@
+<?php 
+
+class Admin {
+
+    protected $userName;
+    protected $ipNumber;
+    
+    function __contruct() {
+
+    }
+
+    function promoteAdmin() {
+
+    }
+
+    function updateProductStock() {
+
+    }
+
+    function updateProductCategory() {
+
+    }
+
+    function sendNewsletter() {
+
+    }
+
+    function addProduct() {
+
+    }
+
+    function removeProduct() {
+        
+    }
+}
+
+?>
---
+++ database/includes/customerClass.php
@@ -0,0 +1,30 @@
+<?php 
+
+class Customer {
+
+    protected $persNr;
+    protected $ipAdress;
+    protected $firstName;
+    protected $LastName;
+    protected $adress;
+    protected $email;
+    protected $phoneNumber;
+    
+    function __construct() {
+
+    }
+
+    function addProductToCart() {
+
+    }
+
+    function confirmOrder() {
+
+    }
+
+    function shippingOption() {
+
+    }
+}
+
+?>
