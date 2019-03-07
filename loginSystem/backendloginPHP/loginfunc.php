46b437b7d81a0ddfc18c0e2aa374bbb1927de6b5
Jesperjensensvard <43741171+Jesperjensensvard@users.noreply.github.com>
2019-03-07 11:38:56 +0100

    loginsystem first commit

---
+++ loginSystem/backendloginPHP/loginfunc.php
@@ -0,0 +1,34 @@
+<?php
+include 'db.php';
+
+// getting user information
+$userName = mysqli_real_escape_string($db, $_POST['username']);
+$password = mysqli_real_escape_string($db, $_POST['password']); 
+
+// grab information form database 
+$resultOfDataBase = $db->query("SELECT UserName,Password,Role FROM users");
+// make an empty array to sen information into
+$UserDataBaseResult = [];
+
+if(mysqli_num_rows($resultOfDataBase)) 
+{
+    while($row = mysqli_fetch_assoc($resultOfDataBase)) {
+        $UserDataBaseResult[] = $row;
+        foreach ($UserDataBaseResult as $user) {
+        
+            
+            if($user["UserName"] == $userName && password_verify($password, $user["Password"])) {
+                 echo "your in";
+                exit();
+            } else {
+                echo "you faild";
+                exit();
+            }
+           
+        }
+    }
+}
+
+
+
+?>
---
+++ loginSystem/dbConn/db.php
@@ -0,0 +1,10 @@
+<?php
+$db = new mysqli('localhost','root','','chmgrupp');
+
+if($db->connect_errno)
+{
+    echo $db->connect_errno. "<br>";
+    die("connection failed");
+}
+
+?>
