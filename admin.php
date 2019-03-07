42f0780a6f89fc396ba11523c894a8c78b388f2d
sjogrenjohan <sjogren_johan@hotmail.com>
2019-03-07 11:51:47 +0100

    added style

--- admin.php
+++ admin.php
@@ -1,29 +1,76 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <script src="JS/main.js"></script>
-</head>
-<body>
+<?php
+    include "html-start.php";
+?>
 
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-2">
+        <ul class="nav nav-pills nav-fill">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Lägg till produkt</a>
+            </li>
+        </ul>
+    </div>
+    <div class="col-lg-2">
+        <ul class="nav nav-pills nav-fill">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Uppdatera produkt</a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="col-lg-2">
+        <ul class="nav nav-pills nav-fill">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Ta bort produkt</a>
+            </li>
+        </ul>
+    </div>
+    <div class="col-lg-2">
+        <ul class="nav nav-pills nav-fill">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nyhetsbrev</a>
+            </li>
+        </ul>
+    </div>
+    <div class="col-lg-2">
+        <ul class="nav nav-pills nav-fill">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Beställningar</a>
+            </li>
+        </ul>
+    </div>
+    <div class="col-lg-2">
+        <ul class="nav nav-pills nav-fill">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Beställningar</a>
+            </li>
+        </ul>
+    </div>
+  </div>
+</div>
+
+<div class="container">
     <form name="addProductForm">
-        ProductID: <input type="text" name="prodID">
-        Namn: <input type="text" name="name">
-        Pris: <input type="text" name="unitPrice">
-        Produktbeskrivning: <input type="text" name="productDesc">
-        Antal produkter: <input type="text" name="unitsInStock">
-        Höjd: <input type="text" name="height">
-        Bredd: <input type="text" name="width">
-        Längd: <input type="text" name="length">
-        Vikt: <input type="text" name="weight">
-        Lägg till bild <!-- kod för ladda upp fil -->
+        <div class="form-group">
+            ProductID: <input type="text" name="prodID" class="form-control" id="formGroupExampleInput">
+            Namn: <input type="text" name="name" class="form-control" id="formGroupExampleInput">
+            Pris: <input type="text" name="unitPrice" class="form-control" id="formGroupExampleInput">
+            Produktbeskrivning: <input type="text" name="productDesc" class="form-control" id="formGroupExampleInput">
+            Antal produkter: <input type="text" name="unitsInStock" class="form-control" id="formGroupExampleInput">
+            Höjd: <input type="text" name="height" class="form-control" id="formGroupExampleInput">
+            Bredd: <input type="text" name="width" class="form-control" id="formGroupExampleInput">
+            Längd: <input type="text" name="length" class="form-control" id="formGroupExampleInput">
+            Vikt: <input type="text" name="weight" class="form-control" id="formGroupExampleInput">
+            Lägg till bild <!-- kod för ladda upp fil -->
+        </div>
     </form>
 
-    <button onclick="addProductDB()">Spara produkt</button>
-    <button onclick="getProduct()">get</button>
-    
-</body>
-</html>
+    <button class="btn btn-primary" onclick="addProductDB()">Lägg till produkt</button>
+    <button class="btn btn-primary" onclick="getProduct()">Hämta produkt</button>
+</div>
+
+<?php
+    include "html-end.php";
+?>
