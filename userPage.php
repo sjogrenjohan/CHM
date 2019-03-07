94cb3dcf5fa3051b53acc30a96b1665f9162faa1
sjogrenjohan <sjogren_johan@hotmail.com>
2019-03-07 15:32:22 +0100

    added userPage

---
+++ userPage.php
@@ -0,0 +1,18 @@
+<?php
+include "./includes/html-start.php";
+include "./includes/header.php";
+?>
+
+<div class="jumbotron">
+    <div class="container">
+        <div id="cartContainer" class="row">
+            
+        </div>
+    </div>
+</div>
+
+<?php
+include "./includes/footer.php";
+include "./includes/html-end.php";
+?>
+
