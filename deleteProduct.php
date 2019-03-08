73ae6deec0894d99550dc65e3e0c4d7ff9d79a0c
sjogrenjohan <sjogren_johan@hotmail.com>
2019-03-08 11:44:13 +0100

    added styles

--- deleteProduct.php
+++ deleteProduct.php
@@ -1,20 +1,69 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <script src="JS/product.js"></script>
-</head>
-<body>
+<?php
+    include "./includes/html-start.php";
+?>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-2">
+        <ul class="nav nav-pills nav-fill">
+            <li class="nav-item">
+                <a class="nav-link" href="./admin.php">Lägg till produkt</a>
+            </li>
+        </ul>
+    </div>
+    <div class="col-lg-2">
+        <ul class="nav nav-pills nav-fill">
+            <li class="nav-item">
+              <a class="nav-link" href="./updateProduct.php">Uppdatera produkt</a>
+            </li>
+        </ul>
+    </div>
 
-    <form name="deleteProduct">
-        Delete product (ProductID): <input type="text" name="prodID">
-    </form>
+    <div class="col-lg-2">
+        <ul class="nav nav-pills nav-fill">
+            <li class="nav-item">
+                <a class="nav-link" href="./deleteProduct.php">Ta bort produkt</a>
+            </li>
+        </ul>
+    </div>
+    <div class="col-lg-2">
+        <ul class="nav nav-pills nav-fill">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nyhetsbrev</a>
+            </li>
+        </ul>
+    </div>
+    <div class="col-lg-2">
+        <ul class="nav nav-pills nav-fill">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Beställningar</a>
+            </li>
+        </ul>
+    </div>
+    <div class="col-lg-2">
+        <ul class="nav nav-pills nav-fill">
+            <li class="nav-item">
+                <a class="nav-link" href="./index.php">Till CHM</a>
+            </li>
+        </ul>
+    </div>
+  </div>
+</div>
 
-    <button onclick="deleteProductDB()">Radera produkt</button>
-    <button onclick="getProduct()">get</button>
-    
-</body>
-</html>
+<div class="container">
+    <div class="row justify-content-md-center">
+        <form name="deleteProduct">
+            <div class="form-group">
+                Delete product (ProductID): <input type="text" name="prodID" class="form-control" id="formGroupExapleInput">
+            </div>
+        </form>
+    </div>
+</div>
+<div class="row justify-content-md-center">
+    <button class="btn btn-primary" onclick="deleteProductDB()">Radera produkt</button>&nbsp;
+    <button class="btn btn-primary" onclick="getProduct()">get</button>
+</div>
+
+
+<?php
+    include "./includes/html-end.php";
+?>
