82876939e2b76a529ff986ee610b4e88d27e39d6
sjogrenjohan <sjogren_johan@hotmail.com>
2019-03-08 09:42:34 +0100

    removed comment

---
+++ DatabaseApi/categoryHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+    class categoryHandler {
+
+        function __construct() {
+            include_once('databaseHandler.php');
+            $this->database = new Database();
+        }
+
+        // Get all students
+        public function getCategory() {
+            $query = $this->database->connection->prepare("SELECT * FROM categories;");
+            $query->execute();
+            $result = $query->fetchAll();
+    
+            if (empty($result)){
+                return array("error"=> "No worky");
+            }
+            return $result;
+        }
+    }
+
+?>
