e5c824d1b51bbce6424f98746a934b5dad806852
Jesperjensensvard <43741171+Jesperjensensvard@users.noreply.github.com>
2019-03-12 11:40:09 +0100

    fixing loginlogoutbutton

--- JS/login.js
+++ JS/login.js
@@ -1,47 +1,49 @@
- // check if user is logged in with fetch
- var checkForUSer = function() {
-   fetch('./loginSystem/logincheck.php', {
-        method: 'get',
-
-    }).then(function(response) {
-            if (response.status >= 200 && response.status < 300) {
-                return response.text()
-            }
-            throw new Error(response.statusText)
-        })
-        .then(function(response) {  
-            if(response != "") {
-                localStorage.setItem('userType', JSON.stringify(response));
-            }
-        })
-}();
-
-function admin() {
-    $(document).ready(function(){
-        $('#adminButton').show();
-    });
-}
-function notadmin() {
-    $(document).ready(function(){
-        $('#adminButton').hide();
-    });  
-}
-
-
-
-
-(function() {
-
-    var getUsertype = JSON.parse(localStorage.getItem('userType'));
-    var getLogginButton = document.getElementById("LogInLogOut");
+$(document).ready(function(){
    
-    
-    if(getUsertype == "admin") {
-      
-        admin();
-    }
-    else{
-        notadmin();
-    }
-
-})();
+    var checkForUSer = function() {
+      fetch('./loginSystem/logincheck.php', {
+           method: 'get',
+   
+       }).then(function(response) {
+               if (response.status >= 200 && response.status < 300) {
+                   return response.text()
+               }
+               throw new Error(response.statusText)
+           })
+           .then(function(response) {  
+               if(response != "") {
+                   localStorage.setItem('userType', JSON.stringify(response));
+               }
+           })
+   }();
+   
+   function admin() {
+     
+           $('#adminButton').show();
+       
+   }
+   function notadmin() {
+       
+           $('#adminButton').hide();
+       
+   }
+   
+   
+   
+   
+   (function() {
+   
+       var getUsertype = JSON.parse(localStorage.getItem('userType'));
+       var getLogginButton = document.getElementById("LogInLogOut");
+       console.log(getLogginButton);
+       
+       if(getUsertype == "admin") {
+           admin();
+           getLogginButton.innerHTML = "Logga ut";
+       }
+       else {
+           notadmin();
+       }
+   
+   })();
+}); 
