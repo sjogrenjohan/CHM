2ed0434278da9a68112aa20ea3be17d3c84314b3
sjogrenjohan <sjogren_johan@hotmail.com>
2019-03-12 14:29:07 +0100

    typo

--- includes/footer.php
+++ includes/footer.php
@@ -16,7 +16,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Premunera på CHM's nyhetsbrev</h5>
+            <h5 class="modal-title" id="exampleModalLabel">Prenumerera på CHM's nyhetsbrev</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
