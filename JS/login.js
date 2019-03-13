f9554767b919dc9d19c9cb0cada5468d743e4121
Jesperjensensvard <43741171+Jesperjensensvard@users.noreply.github.com>
2019-03-13 09:11:23 +0100

    complete login system

--- JS/login.js
+++ JS/login.js
@@ -1,49 +1,70 @@
+
 $(document).ready(function(){
-   
+
+
     var checkForUSer = function() {
-      fetch('./loginSystem/logincheck.php', {
-           method: 'get',
-   
-       }).then(function(response) {
-               if (response.status >= 200 && response.status < 300) {
-                   return response.text()
-               }
-               throw new Error(response.statusText)
-           })
-           .then(function(response) {  
-               if(response != "") {
-                   localStorage.setItem('userType', JSON.stringify(response));
-               }
-           })
-   }();
-   
-   function admin() {
-     
-           $('#adminButton').show();
-       
-   }
-   function notadmin() {
-       
-           $('#adminButton').hide();
-       
-   }
-   
-   
-   
-   
-   (function() {
-   
-       var getUsertype = JSON.parse(localStorage.getItem('userType'));
-       var getLogginButton = document.getElementById("LogInLogOut");
-       console.log(getLogginButton);
-       
-       if(getUsertype == "admin") {
-           admin();
-           getLogginButton.innerHTML = "Logga ut";
-       }
-       else {
-           notadmin();
-       }
-   
-   })();
+        fetch('./loginSystem/logincheck.php', {
+            method: 'get',
+
+        }).then(function(response) {
+                if (response.status >= 200 && response.status < 300) {
+                    return response.text()
+                }
+                throw new Error(response.statusText)
+            })
+            .then(function(response) {  
+                if(response != "") {
+                    localStorage.setItem('userType', JSON.stringify(response));
+                }
+            })
+    }();
+
+
+    function admin() {    
+            $('#adminButton').show();        
+    }
+
+    function notadmin() {      
+            $('#adminButton').hide();      
+    }
+
+
+
+    (function() {
+        
+        var getUsertype = JSON.parse(localStorage.getItem('userType'));
+        
+        if(getUsertype == "admin") {
+            admin();
+            
+        }   
+        else if(getUsertype == "not admin"){
+            notadmin();
+            
+        } else {
+            notadmin();              
+        }
+        
+    })();
+
+
 }); 
+
+function jagGerUpp() {
+
+    fetch('./loginSystem/logOut.php', {
+        method: 'post',
+    }).then(function(X) {
+        if (X.status >= 200 && X.status < 300) {
+            return X.text()
+        }
+        throw new Error(X.statusText)
+    })
+    .then(function(X) {  
+        
+        
+    }).catch(function(err) {
+        console.error(err)
+    })
+
+}   
