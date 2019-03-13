62b13df4574f6de06c4db7bf78377d1bee4d293c
sjogrenjohan <sjogren_johan@hotmail.com>
2019-03-13 14:57:14 +0100

    added some content to confirm order

--- cartPage.php
+++ cartPage.php
@@ -35,13 +35,63 @@ include "./includes/header.php";
             <div class="col-6 col-md-4">
             </div>
             <div class="col-6 col-md-4" style="display: flex; justify-content: center;">            
-                <button type="button" class="btn btn-outline-danger" onclick="confirmBuy()">Slutför köp</button>
+                <button data-toggle="modal" data-target="#exampleModalCenter" data-whatever="@mdo" type="button" class="btn btn-outline-danger" onclick="confirmBuy()">Slutför köp</button>
+
             </div>
             <div class="col-6 col-md-4" style="display: flex; justify-content: flex-end;">
                 <p>Totalt pris</p>
             </div>
         </div>
     </div>
+</div>
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">Slutför din betalning</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <div class="modal-body">
+        <label for="recipient-name" class="col-form-label">OBS: Din faktura leveras via epost.</label>
+        <form name="confirmPayment">
+            <div class="form-group">
+                <label for="recipient-name" class="col-form-label">Namn:</label>
+                <input type="text" name="name" class="form-control" id="recipient-name">
+            </div>
+            <div class="form-group">
+                <label for="message-text" class="col-form-label">Email:</label>
+                <input type="text" name="email" class="form-control" id="recipient-name">
+            </div>
+            <div class="form-group">
+                <label for="message-text" class="col-form-label">Adress:</label>
+                <input type="text" name="adress" class="form-control" id="recipient-name">
+            </div>
+            <label for="message-text" class="col-form-label">Fraktalternativ:</label>
+            <div class="form-check">
+                <input name="homeDelivery" class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                <label class="form-check-label" for="defaultCheck1">
+                    Hemleverans
+                </label>
+                </div>
+            <div class="form-check">
+                <input name="getInStore" class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                <label class="form-check-label" for="defaultCheck1">
+                    Hämta i butik
+                </label>
+                </div>
+            <div class="form-group">
+                <label for="recipient-name" class="col-form-label">Vill du ha vårat nyhetsbrev? <span data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo" style="cursor: pointer;">Klicka här</span></label>
+            </div>
+            </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-dark" data-dismiss="modal">Ångra</button>
+        <button type="button" class="btn btn-dark">Bekräfta</button>
+      </div>
+    </div>
+  </div>
 </div>
 <script src="./JS/cart.js"></script>
 <script>
