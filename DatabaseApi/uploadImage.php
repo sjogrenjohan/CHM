fde4e12e4267559bdf83916148bd275ab3f9d899
sjogrenjohan <sjogren_johan@hotmail.com>
2019-03-14 15:48:15 +0100

    upload image to folder function

---
+++ DatabaseApi/uploadImage.php
@@ -0,0 +1,56 @@
+<?php
+
+function saveImage($file) {
+    try {
+        $target_dir = "../productImages/";
+        $target_file = $target_dir . basename($file["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+        // Check if image file is a actual image or fake image
+        $check = getimagesize($file["tmp_name"]);
+        if($check !== false) {
+            //return "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            return "File is not an image.";
+            $uploadOk = 0;
+        }
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            return "Sorry, file already exists.";
+            $uploadOk = 0;
+        }
+        
+        // Check file size
+        if ($file["size"] > 500000) {
+            return "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+        
+        // Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+            return "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+        
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            return "Sorry, your file was not uploaded.";
+            // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($file["tmp_name"], $target_file)) {
+                return basename($file["name"]);
+            } else {
+                return "Sorry, there was an error uploading your file.";
+            }  
+        } 
+        
+        
+    } catch (Exception $e) {
+        throw $e;
+    }
+    
+}
+
+?>
