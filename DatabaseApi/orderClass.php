90377ab8a09d375614415f4bc6aed0abb3f8c50c
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-03-14 15:21:45 +0100

    add order class

---
+++ DatabaseApi/orderClass.php
@@ -0,0 +1,20 @@
+<?php
+
+class Order {
+
+    function __construct() {
+        include_once('databaseHandler.php');
+        $this->database = new Database();
+    }
+
+    public function addOrderInfo() {
+        $query = $this->database->connection->prepare("SELECT  * FROM orders;");
+        $query->execute();
+        $result = $query->fetchAll();
+
+        if (empty($result)){
+            return array("error"=> "Finns ej några registrerade nyhetsbrevskunder");
+        }
+        return $result;
+    }
+}
