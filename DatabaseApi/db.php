f79d687f6ac704fb84f98531f55c420b854d4d91
samer <samer.senbol@gmail.com>
2019-05-17 16:01:35 +0100

    Fixing connecting to database.

--- DatabaseApi/db.php
+++ DatabaseApi/db.php
@@ -1,10 +1,20 @@
 <?php
-$db = new mysqli('my77b.sqlserver.se','235426_pm13163','Munkjacka56','235426-chmgrupp7');
+
+$connection = mysqli_connect('localhost', 'root', '', 'chmgrupp7_0');
+
+    if($connection){
+        echo"You are connected";
+    }
+    else{
+        die("Database connection is faild");
+        }
+
+/* $db = new mysqli('my77b.sqlserver.se','235426_pm13163','Munkjacka56','235426-chmgrupp7');
 
     if($db->connect_errno)
     {
         echo $db->connect_errno. "<br>";
         die("connection failed");
-    }
+    } */
 
 ?>
