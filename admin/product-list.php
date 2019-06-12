3bc6bf395fc26648140e8122b8d82c8f962997a3
samer <samer.senbol@gmail.com>
2019-06-12 14:18:32 +0200

    Fixing the qurey syntax.

--- admin/product-list.php
+++ admin/product-list.php
@@ -2,7 +2,7 @@
     include "../includes/admin-start.php";
     include "../includes/adminHeader.php";
 
-    error_reporting(0); 
+    //error_reporting(0); 
     include '../DatabaseApi/db.php';
 ?>
 
@@ -11,23 +11,24 @@
     <thead>
         <tr>
             <th>Product ID</th>
-            <th>categories Name</th>
-            <th> product Name</th>
+            <th>Product Name</th>
+            <th>Categories Name </th>
+            
         </tr>
     </thead>
 
     <tbody>
 
 
- <!--    select *
-from
-    tableA a
-        inner join
-    tableB b
-        on a.common = b.common
-        inner join 
-    TableC c
-        on b.common = c.common -->
+
+<!-- <select>  
+  <option value="Select">Select</option> 
+  <option value="Grafikkort">Grafikkort</option>  
+  <option value="Hårddiskar">Hårddiskar</option>  
+  <option value="Moderkort">Moderkort</option>  
+  <option value="Processor">Processor</option>  
+  <option value="RAM">RAM</option>  
+</select> -->   
 
 
 
@@ -36,25 +37,64 @@ from
         FROM products 
             INNER JOIN
         category_relations 
-        ON products.ProductID = category_relations.CategoryID
+        ON products.ProductID = category_relations.ProductID
             INNER JOIN 
         categories 
-            ON category_relations.ProductID = categories.CategoryID
+            ON category_relations.CategoryID = categories.CategoryID
         ";
+
+        /* $selectBox = '<select>';
+        $queryC = "SELECT * FROM categories";
+        $selct_category = mysqli_query($db, $queryC);
+        while ($row = mysqli_fetch_assoc($selct_category)) {
+            $categoryName = $row ['categoryName'];
+            $categoryID = $row ['CategoryID']; */
+            /* echo"<tr>";
+                echo"<td>$ProductID</td>";
+            echo"</tr>"; */
+     /*        if(isset($_GET['Grafikkort'])){
+                $categoryName =$_GET['Grafikkort'];
+                $queryC="UPDATE categories SET categoryName ='Grafikkort' WHERE CategoryID=$categoryID";
+                $change_to_admin_query=mysqli_query($db,$queryC);
+                header("Location: product-list.php"); 
+        }
+     */    
 
         $select_products = mysqli_query($db, $query);
         while($row = mysqli_fetch_assoc($select_products)){
+
             $ProductID = $row ['ProductID'];
-            $Name = $row ['Name'];
             $productName = $row ['productName'];
             $categoryName = $row ['categoryName'];
+            $CategoryIDin = $row ['CategoryID'];
 
             echo"<tr>";
                 echo"<td>$ProductID</td>";
-                echo"<td>$Name</td>";
-                echo"<td>$categories</td>";
+                echo"<td>$productName</td>";
+                echo"<td>$categoryName";
+                echo '<form>';
+                    echo "<select name = 'categorySave'>";
+                        $queryC = "SELECT * FROM categories";
+                        $selct_category = mysqli_query($db, $queryC);
+
+                        while ($rowC = mysqli_fetch_assoc($selct_category)){
+                            $selected = "";
+                            
+                            $categoryName = $rowC ['categoryName'];
+                            $categoryID = $rowC ['CategoryID'];
+                            if($CategoryIDin == $categoryID ){
+                                $selected =  "selected";
+                            }
+                            echo"<option value =".$categoryID." ".$selected.">".$categoryName."</option>";
+                            //echo $categoryID;
+                        }
+                    echo"</select>";
+                    echo"<bottun type = 'submit'> Spara </bottun>";
+                    echo '</form>';
+                echo"</td>";
             echo"</tr>";
         }
+        
         ?>
     </tbody>
 </table>
